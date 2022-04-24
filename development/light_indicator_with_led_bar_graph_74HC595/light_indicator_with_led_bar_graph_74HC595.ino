7c0454edeada256b6b4d40468f161aea7ca2bc01
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:35:25 +0300

    Change.

--- development/light_indicator_with_led_bar_graph_74HC595/light_indicator_with_led_bar_graph_74HC595.ino
+++ development/light_indicator_with_led_bar_graph_74HC595/light_indicator_with_led_bar_graph_74HC595.ino
@@ -1,7 +1,7 @@
 /*
  *  Light Indicator (LEDs Bar Graph & 74HC595 8-bit SR).
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
